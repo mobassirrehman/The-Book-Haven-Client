e1901c4d318e090ef96ba4f28a5d4f9b4dff1eb4
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-11-10 13:55:34 +0600

    routes and main added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from "../layouts/Root";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AllBooks from "../pages/AllBooks";
+import AddBook from "../pages/AddBook";
+import MyBooks from "../pages/MyBooks";
+import BookDetails from "../pages/BookDetails";
+import UpdateBook from "../pages/UpdateBook";
+import ErrorPage from "../pages/ErrorPage";
+import NotFound from "../pages/NotFound";
+import PrivateRoute from "../privateRoute/PrivateRoute";
 
-const routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default routes;
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/all-books",
+        element: <AllBooks />,
+      },
+      {
+        path: "/add-book",
+        element: (
+          <PrivateRoute>
+            <AddBook />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/my-books",
+        element: (
+          <PrivateRoute>
+            <MyBooks />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/book/:id",
+        element: (
+          <PrivateRoute>
+            <BookDetails />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/update-book/:id",
+        element: (
+          <PrivateRoute>
+            <UpdateBook />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
